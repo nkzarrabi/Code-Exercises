9364bd85bee3e36d62f1da3018b5d93ca8555e02
nkzarrabi <48893251+nkzarrabi@users.noreply.github.com>
2025-01-09 11:39:17 -0500

    Day 23

---
+++ aoc-2024/Day_23/d23.py
@@ -0,0 +1,12 @@
+flag = False
+file = 'example.txt' if flag else 'input.txt'
+connections = [{connection[:2], connection[3:]} for connection in open(file).read().split()]
+total = 0
+for i in range(len(connections) - 2):
+    for j in range(i + 1, len(connections) - 1):
+        lan = connections[i] | connections[j]
+        if len(lan) == 3:
+            for k in range(j + 1, len(connections)):
+                if connections[k].issubset(lan) and 't' in {computer[0] for computer in connections[i] | connections[j] | connections[k]}:
+                    total += 1
+print(total)
